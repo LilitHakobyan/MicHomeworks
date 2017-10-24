94dc3f8eb62f4ca670206f1c9d20c0c5a5d5b820
lilithakobyan <lilithakobyan1994@mail.ru>
2017-10-24 16:43:48 +0400

    homwork1

---
+++ HomworkOneInheritance/HomworkOneInheritance/Figures.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomworkOneInheritance
+{
+    class Figures
+    {  public double Width  {get; set;}
+       public double Height { get; set;}
+
+        public Figures(double height, double width)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+
+    class Rectangular:Figures
+    {
+        public Rectangular(double height, double width) : base(height, width)
+        {
+            
+        }
+        public void Drow()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+    class Triangular : Figures
+    {
+        public Triangular(double height, double width) : base(height, width)
+        {
+
+        }
+        public void Drow()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j <i+1; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+    class Quadrate : Figures
+    {
+        public Quadrate(double size) : base(size, size)
+        {
+
+        }
+        public void Drow()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ HomworkOneInheritance/HomworkOneInheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomworkOneInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangular rect= new Rectangular(4,6);
+            rect.Drow();
+            Console.WriteLine("----------------------------------");
+            Triangular triangular = new Triangular(4,6);
+            triangular.Drow();
+            Console.WriteLine("----------------------------------");
+
+            Quadrate quadrate =new Quadrate(6);
+            quadrate.Drow();
+            Console.WriteLine("----------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
