bb60197a77233b8fa595218dc67d7bfe45d4cf28
lilithakobyan <lilithakobyan1994@mail.ru>
2017-11-02 15:51:24 +0400

    array test

---
+++ ArrayTest/ArrayTest/DynamicArray.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+   public class DynamicArray
+    {
+        private int[] innerArray;
+
+        public DynamicArray()
+        {
+            innerArray = new int[1];
+        }
+
+        public DynamicArray(int[] arr)
+        {
+            innerArray = arr;
+        }
+
+        public void Add(int item)
+        {
+            int[] tempArr = new int[innerArray.Length + 1];
+            for (int i = 0; i < innerArray.Length; i++)
+            {
+                tempArr[i] = innerArray[i];
+            }
+            tempArr[innerArray.Length] = item;
+            innerArray = tempArr;
+
+        }
+
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < innerArray.Length; i++)
+            {
+                if (innerArray[i] == item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int Find(int item)
+        {
+            if (Contains(item))
+            {
+                for (int i = 0; i < innerArray.Length; i++)
+                {
+                    if (innerArray[i]==item)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public void Remove(int item)
+        {
+            int[] tempArr = new int[innerArray.Length - 1];
+            int index = Find(item);
+            if (index!=-1)
+            {
+                for (int i = 0; i < innerArray.Length; i++)
+                {
+                    if (i==index)
+                    {
+                        for (int j = i; j < tempArr.Length; j++)
+                        {
+                            tempArr[j] = innerArray[j+1];
+                        }
+                        break;
+                    }
+                    tempArr[i] = innerArray[i];
+                }
+
+            }
+            innerArray = tempArr;
+        }
+
+        public void ShowOnConsole()
+        {
+            for (int i = 0; i < innerArray.Length; i++)
+            {
+                Console.Write(innerArray[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
---
+++ ArrayTest/ArrayTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {4, 5, 3, 9, 4, 12, 78, 3, 57, 1};
+            DynamicArray array=new DynamicArray(arr);
+            Console.WriteLine(array.Find(3));
+            array.Add(15);
+            array.ShowOnConsole();
+            Console.WriteLine(array.Contains(0));
+            array.Remove(12);
+            array.ShowOnConsole();
+            Console.ReadKey();
+        }
+    }
+}
