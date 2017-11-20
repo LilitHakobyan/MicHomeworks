c8ad70e1e5d9306bba8805d1f7ce7333bcbac6b1
lilithakobyan <lilithakobyan1994@mail.ru>
2017-11-20 21:31:27 +0400

    ColorAttribute

---
+++ Attributes/Attributes/ColorAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class ColorAttribute:Attribute
+    {
+        public ConsoleColor Color { get; set; }
+
+        public ColorAttribute()
+        {
+
+        }
+
+        public ColorAttribute(ConsoleColor color)
+        {
+            Color = color;
+        }
+    }
+}
---
+++ Attributes/Attributes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    [Color(Color = ConsoleColor.Blue)]
+    class ColorClass
+    {
+        public override string ToString()
+        {
+            return "Hello World";
+        }
+    }
+    class Program
+    {
+        static void ShowOnConsole(ColorClass c)
+        {
+            ColorAttribute ca =
+                Attribute.GetCustomAttribute(c.GetType(),
+                    typeof(ColorAttribute)) as ColorAttribute;
+
+            ConsoleColor newColor;
+            if (ca == null)
+                newColor = Console.ForegroundColor;
+            else
+                newColor = ca.Color;
+
+            ConsoleColor currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = newColor;
+            Console.WriteLine(c);
+            Console.ForegroundColor = currentColor;
+        }
+        static void Main(string[] args)
+        {
+            ShowOnConsole(new ColorClass());
+        }
+    }
+}
