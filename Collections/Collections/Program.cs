6c1241b387f45920eaeed743ef70cdb7ec8ebcc6
lilithakobyan <lilithakobyan1994@mail.ru>
2018-03-04 22:39:39 +0400

    realization IEnumerator and IEnumerable

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserCollection users=new UserCollection();
+            //foreach (var user in users)
+            //{
+            //    Console.WriteLine(user.ToString());
+            //}
+
+            IEnumerable enumerable=users as IEnumerable;
+
+            IEnumerator enumerator = users.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                User user = enumerator.Current as User;
+                Console.WriteLine(user.ToString());
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            foreach (var u in users)
+            {
+                Console.WriteLine(u.ToString());
+            }
+        }
+    }
+}
---
+++ Collections/Collections/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"User Name is {this.Name} and age {this.Age}";
+        }
+    }
+}
